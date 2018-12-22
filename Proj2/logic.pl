5a046cb39e4da8c142aefde3088127159fdef680
wortz <jp_pp_hi5@hotmail.com>
2018-12-22 00:30:33 +0000

    almost done

--- Proj2/logic.pl
+++ Proj2/logic.pl
@@ -27,41 +27,74 @@ func:-
     
     actividadesMeses(ActividadesMeses),
     
-    iter(Matrix,1,1,1,Matrix,_),nl,nl,
-    write(Matrix). 
+    iterAux(Matrix,1),
+    iterarfinal(Matrix,1,NrActividades),
+    flattenLists(Matrix,FlattenedMatrix),
+   
+    labeling([],FlattenedMatrix),
+    nl,nl,
+
+    write(FlattenedMatrix).
 
 
-iter([],IndexMes,IndexInvestigador,_,Matrix,SumMes):-  %%passar 2 vezes a Matrix?? acho q tem de ser
-    duracaoProjecto(Dur),
+
+
+%%INVESTIGATORS
+%%itera por todos os investigadores
+%%iterAux(Matrix,IndexInvestigador)
+%%Matrix- Matrix3d com todos os meses , investigadores e atividades
+%%IndexInvestigador- Indice do investigador a ser avaliado
+iterAux(Matrix,IndexInvestigador):-
     nrInvestigadores(NrInv),
-    ((
-        IndexMes1 is IndexMes +1,
-        IndexMes1=<Dur,
-        SumMes=0,
-        iter(Matrix,IndexMes1,IndexInvestigador,1,Matrix,SumMes1)
-    )
-    ;(  
-        IndexInvestigador1 is IndexInvestigador+1,  %%chega ao fim dos meses, checka outro inv
-        (
-            IndexInvestigador1 =< NrInv,
-            iter(Matrix,1,IndexInvestigador1,1,Matrix,SumMes)
-        )
-            ;true
-    )).
-iter([H|T],IndexMes, IndexInvestigador,IndexActivity,Matrix,SumMes):-
+    iterAux1(Matrix,IndexInvestigador,1,SumTotal),
+    restricaoTempoDedicarProjeto(IndexInvestigador,SumTotal),
+    ((IndexInvestigador1 is IndexInvestigador+1,
+    IndexInvestigador1 =< NrInv,
+    iterAux(Matrix,IndexInvestigador1));
+    true).
+
+
+%%MESES
+%%itera por todos os meses de trabalho de um investigador
+%%iterAux1(Matrix,IndexInvestigador, IndexMes,SumTotal)
+%%Matrix- Matrix3d com todos os meses , investigadores e atividades
+%%IndexMes- Indice do mes a avaliar
+%%IndexInvestigador- Indice do investigador a ser avaliado
+%%SumTotal- Soma do tempo total que ele trabalha no projeto
+iterAux1(Matrix,IndexInvestigador,IndexMes,SumTotal):-
+    duracaoProjecto(Dur),
+    iter(Matrix,IndexMes,IndexInvestigador,1,SumMes),  %%SumMes é o total de horas que um investigador trabalha num mes
+    checkInvestigador(IndexInvestigador,SumMes,1),
+    ((IndexMes1 is IndexMes +1,
+    IndexMes1=<Dur,
+    iterAux1(Matrix,IndexInvestigador,IndexMes1,SumTotal1),
+    SumTotal #= SumMes + SumTotal1);
+    true).
+
+
+%%ACTIVIDADES
+%%itera por certo mes de todas as atividades para um investigador
+%%iter(Matrix,IndexMes, IndexInvestigador,IndexActivity,SumMes)
+%%Matrix- Matrix3d com todos os meses , investigadores e atividades
+%%IndexMes- Indice do mes a avaliar
+%%IndexInvestigador- Indice do investigador a ser avaliado
+%%IndexActivity- Indice da atividade
+%%SumMes- Soma do tempo que ele trabalha por mes em todas as atividades
+iter([],_,_,_,0).  
+iter([H|T],IndexMes, IndexInvestigador,IndexActivity,SumMes):-
     nth1(IndexInvestigador,H,CurrentInvest_Activity),%%encontra meses de investigador na actividade a ser iterada
     nth1(IndexMes,CurrentInvest_Activity,M),
     a(M,IndexActivity,IndexInvestigador,IndexMes),
     IndexActivity1 is IndexActivity+1,
-    iter(T,IndexMes, IndexInvestigador,IndexActivity1,Matrix,SumMesAux).
-    SumMes #= SumMesAux + M,
-    checkInvestigador(IndexInvestigador,SumMes).
+    iter(T,IndexMes, IndexInvestigador,IndexActivity1,SumMes1),
+    SumMes #= SumMes1 + M.
 
 %%params
 %%Index do invest
 %% Funçao pode ser usada para restringir a soma das horas de trabalho
 %de um investigador num mes, ou para restringir as horas de um mes numa acti
-checkInvestigador(IndexInvestigador,SumMes_ou_MaxMes):-
+%%caso boolean =1 a variavel SumMes_ou_MaxMes é SumMes ,caso 2 é MaxMes
+checkInvestigador(IndexInvestigador,SumMes_ou_MaxMes,Boolean):-
     (
         docente(X),member(IndexInvestigador,X),      
         docenteMaxHorasMensais(IndexInvestigador,HorasMesDocente),
@@ -70,11 +103,16 @@ checkInvestigador(IndexInvestigador,SumMes_ou_MaxMes):-
     (
         contratado(Y),member(IndexInvestigador,Y),
         contratadoHorasMensais(IndexInvestigador,HorasMesContratado),
-        SumMes_ou_MaxMes #= HorasMesContratado
+        ((Boolean=1,
+        SumMes_ou_MaxMes #= HorasMesContratado);
+        SumMes_ou_MaxMes in 0..HorasMesContratado)
     ).
 
 
-
+restricaoTempoDedicarProjeto(IndexInvestigador,SumTotal):-
+    ((docenteTotalHorasProjecto(IndexInvestigador,HorasADedicar),
+    SumTotal #=HorasADedicar);
+    true).
 
 
 %params
@@ -86,10 +124,10 @@ a(M,IndexActivity,IndexInvestigador,IndexMes):-
         (actividadesMeses(ActividadesMeses),
         nth1(IndexActivity,ActividadesMeses,CurrentActivityMeses),
         \+ member(IndexMes,CurrentActivityMeses))),
-        M=0
+        M#=0
       )
     ;(
-        checkInvestigador(IndexInvestigador,M)
+        checkInvestigador(IndexInvestigador,M,2)
     ).
 
 
@@ -141,3 +179,49 @@ retricoes_rigidas1(IndexInvestigador,MatrixH):-
     ).
 
 
+
+
+%%Iterar sobre NrActividades e passar esse valor no param CurrentActivity
+iterarfinal(Matrix,IndexActivity,NrAtividades):-
+    nth1(IndexActivity,Matrix,CurrentActivityMatrix),
+    iterInvestigadores(CurrentActivityMatrix,SumMeses),
+    duracaoActividades(X),nth1(IndexActivity,X,HorasNecessarias),
+    SumMeses #= HorasNecessarias,
+    ((IndexActivity1 is IndexActivity + 1,
+    IndexActivity1 =< NrAtividades,
+    iterarfinal(Matrix,IndexActivity1,NrAtividades));
+    true).
+
+
+%params
+%%indice da actividade a verificar
+%%lista de ActividadesMeses da current actividade
+%%lista da matrix da current actividade
+%%Indice da CurrentActivityMatrix a ser verificado
+iterInvestigadores([],0).
+iterInvestigadores([H|T],SumMeses):-
+    iterSomaInv(H,SomaInv),
+    iterInvestigadores(T,SumMeses1),
+    SumMeses #= SomaInv + SumMeses1.
+
+
+iterSomaInv([],0).
+iterSomaInv([H|T],SomaInv):-
+    iterSomaInv(T,SomaInv1),
+    SomaInv #= H + SomaInv1.
+
+
+
+
+flattenLists(List, NewList):-
+        flattenList(List, NewListAux),
+        flattenList(NewListAux, NewList).
+
+flattenList([], []).
+flattenList([H|T], NewList):-
+        flattenList(T, Prev),
+        pushElementsToList(H, Prev, NewList).
+
+pushElementsToList([], R, R).
+pushElementsToList([H|T], Prev, [H|NewList]):-
+    pushElementsToList(T, Prev, NewList).
